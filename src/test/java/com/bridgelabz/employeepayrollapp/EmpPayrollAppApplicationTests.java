bcfb0b6e1772438c5f01296477740229f73b440c
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-10-10 15:17:32 +0530

    ADDED : Conection with DB and Started project

---
+++ src/test/java/com/bridgelabz/employeepayrollapp/EmpPayrollAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpPayrollAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
