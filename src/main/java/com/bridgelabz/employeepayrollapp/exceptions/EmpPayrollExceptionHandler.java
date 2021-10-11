b521eaa64b86d9d7fda0dc764fa7aafbfaac3382
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-10-11 21:40:15 +0530

    ADDED : Controller Advice and Handled exception at controller level

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/exceptions/EmpPayrollExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.employeepayrollapp.exceptions;
+
+import com.bridgelabz.employeepayrollapp.dto.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class EmpPayrollExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(
+                                        MethodArgumentNotValidException e) {
+        List<ObjectError> errList = e.getBindingResult().getAllErrors();
+        List<String> errMsg = errList.stream()
+                                     .map(objErr -> objErr.getDefaultMessage())
+                                     .collect(Collectors.toList());
+        ResponseDto responseDto =
+                new ResponseDto("Exception while processing REST Request", errMsg);
+        return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
+    }
+}
